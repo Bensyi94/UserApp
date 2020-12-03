4589a15c18d9fd9c547c90d2e74dd8923f9c9d3b
Bensyi94 <reedektblt@gmail.com>
2020-12-03 13:49:15 -0300

    v0.3

--- app/src/main/java/com/example/userapp/MainActivity.java
+++ app/src/main/java/com/example/userapp/MainActivity.java
@@ -2,13 +2,40 @@ package com.example.userapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button ing;
+    Button reg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Intent activityBienvenido = new Intent(this, Bienvenido.class);
+        Intent activityRegistro = new Intent(this, Registro.class);
+
+        ing = (Button) findViewById(R.id.btnLogin);
+        reg = (Button) findViewById(R.id.btnNewAcc);
+
+
+        ing.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(activityBienvenido);
+            }
+        });
+
+        reg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(activityRegistro);
+            }
+        });
     }
 }
